4661de1d2fc027e06f557aef3b0269a26eec7dd4
Vadim Chorrny <chorrny228@gmail.com>
2022-02-04 10:12:43 +0200

    first commit

---
+++ SushiSite/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SushiSite.Models;
+
+namespace SushiSite.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AccountController : Controller
+    {
+        //private readonly UserManager<User> _userManager;
+        //private readonly SignInManager<User> _signInManager;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SushiSite/Controllers/CartController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using SushiSite.Data;
+using SushiSite.Helpers;
+using SushiSite.Models;
+using SushiSite.Models.ViewModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SushiSite.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IViewRender _viewRender;
+        private readonly IHttpContextAccessor _session;
+
+        public CartController(ApplicationDbContext context, 
+            IEmailSender emailSender, 
+            IViewRender viewRender,
+            IHttpContextAccessor session)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            _viewRender = viewRender;
+            _session = session;
+        }
+        
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            CartListViewModel viewModel = new CartListViewModel()
+            {
+                Foods = GetFoodsFromSession()
+            };
+            return View(viewModel);
+        }
+        private IEnumerable<Food> GetFoodsFromSession()
+        {
+            List<ShoppingOrder> products = HttpContext.Session.GetObject<List<ShoppingOrder>>("ShoppingOrders");
+            if (products == null)
+                products = new List<ShoppingOrder>();
+
+            int[] productIds = products.Select(i => i.FoodId).ToArray();
+
+            return _context.Foods.Where(c => productIds.Contains(c.Id));
+        }
+
+        public IActionResult ClearCart()
+        {
+            _session.HttpContext.Session.Clear();
+            return RedirectToRoute(new { controller = "Home", action = "Index" });
+        }
+
+        // no need
+        public IActionResult Confirm()
+        {
+            //string userEmail = User.Identity.Name;
+            //var items = GetFoodsFromSession();
+
+            //var html = this._viewRender.Render("Mails/OrderSummary", new OrderSummaryMail
+            //{
+            //    UserName = userEmail,
+            //    Foods = items,
+            //    TotalPrice = items.Sum(i => i.Price)
+            //});
+
+            //_emailSender.SendEmailAsync(userEmail, "Order Summary", html);
+
+            return View();
+        }
+    }
+}
